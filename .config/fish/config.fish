7d7ae250a1f1d3dd4ecaca75157639396037c554
Collin Dutter <collindutter@gmail.com>
2023-04-20 10:55:33 -0700

    feat: remove tmux auto-attach

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,13 +23,3 @@ if type -q jenv
 	set PATH $HOME/.jenv/bin $PATH
 	status --is-interactive; and jenv init - | source
 end
-
-# Adapted from https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332626369
-# only run in interactive (not automated SSH for example)
-if status is-interactive
-# don't nest inside another tmux
-and not set -q TMUX
-  # Adapted from https://unix.stackexchange.com/a/176885/347104
-  # Create session 'main' or attach to 'main' if already exists.
-  tmux new-session -A -s main
-end
