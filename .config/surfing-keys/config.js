0f5b00bf02230942db2073e65eeae038ddda00bf
Collin Dutter <collindutter@gmail.com>
2024-03-28 14:25:56 -0600

    Remove surfing keys

--- .config/surfing-keys/config.js
+++
@@ -1,7 +0,0 @@
-api.map('s', 'f'); // Highlight links
-api.map('<Ctrl-u>', 'u'); // Scroll up half a page
-api.map('<Ctrl-d>', 'd'); // Scroll down half a page
-api.map('<Ctrl-i>', 'D'); // Forward in history
-api.map('<Ctrl-o>', 'S'); // Backward in history
-api.imap('<Ctrl-e>', "<Ctrl-i>"); // Open editor in input field 
-api.imap('jk', "<Esc>"); // Escape in input field
