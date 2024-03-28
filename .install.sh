3bbe6fcf1fea4928b1614ede6e316df417d57390
Collin Dutter <collindutter@gmail.com>
2024-03-28 14:25:37 -0600

    Update install script

--- .install.sh
+++ .install.sh
@@ -23,9 +23,15 @@ brew install gnu-sed
 brew install direnv
 brew install font-jetbrains-mono-nerd-font
 
+# Sketchybar
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
+brew install --cask sf-symbols
+brew tap FelixKratz/formulae
+brew install sketchybar
 
 echo "Switching to fish shell..."
-sudo echo /usr/local/bin/fish >> /etc/shells
+sudo echo /usr/local/bin/fish >>/etc/shells
 chsh -s /usr/local/bin/fish
 
 # Update macOS Settings
@@ -46,7 +52,8 @@ defaults write com.apple.Finder AppleShowAllFiles -bool true
 echo "Installing asdf..."
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
 echo source ~/.asdf/asdf.fish
-echo mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+echo mkdir -p ~/.config/fish/completions
+and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 
 echo "Installing asdf plugins..."
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
@@ -90,7 +97,7 @@ git config --global user.email "collindutter@gmail.com"
 
 echo "Configuring dotfiles..."
 alias --save dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-echo ".dotfiles" >> .gitignore
+echo ".dotfiles" >>.gitignore
 git clone --bare https://github.com/collindutter $HOME/.dotfiles
 dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
@@ -100,7 +107,7 @@ tide configure --auto --style=Lean --prompt_colors='True color' --show_time='12-
 
 echo "Configuring python virtualenvs..."
 mkdir .virtualenvs
-python -m venv .virtualenvs/py3nvim 
+python -m venv .virtualenvs/py3nvim
 ./virtualenvs/bin/pip install pynvim
-python -m venv .virtualenvs/debugpy 
+python -m venv .virtualenvs/debugpy
 ./virtualenvs/bin/pip install debugpy
