fc58c937afdb9aa2477e000a86885bd16de4f89b
Collin Dutter <collin@griptape.ai>
2024-10-07 23:09:50 -0700

    Use brewfile, move tmux conf, update install

--- .install.sh
+++ .install.sh
@@ -10,25 +10,9 @@ echo "Installing Homebrew..."
 brew analytics off
 
 echo "Installing Homebrew packages..."
-brew tap homebrew/cask-fonts
-brew install neovim
-brew install jq
-brew install ripgrep
-brew install fd
-brew install tmux
-brew install fish
-brew install yabai
-brew install skhd
-brew install gnu-sed
-brew install direnv
-brew install font-jetbrains-mono-nerd-font
-
-# Sketchybar
-brew tap homebrew/cask-fonts
-brew install --cask font-hack-nerd-font
-brew install --cask sf-symbols
-brew tap FelixKratz/formulae
-brew install sketchybar
+brew update
+brew bundle install --cleanup --file=~/.config/brewfile/Brewfile --no-lock
+brew upgrade
 
 echo "Switching to fish shell..."
 sudo echo /usr/local/bin/fish >>/etc/shells
@@ -49,12 +33,6 @@ defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
 defaults write com.apple.Finder AppleShowAllFiles -bool true
 
 # Install asdf
-echo "Installing asdf..."
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0
-echo source ~/.asdf/asdf.fish
-echo mkdir -p ~/.config/fish/completions
-and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
-
 echo "Installing asdf plugins..."
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
@@ -67,7 +45,7 @@ asdf plugin add python
 asdf global python latest
 
 echo "Installing poetry..."
-curl -sSL https://install.python-poetry.org | python3 -
+pipx install poetry
 
 echo "Installing tpm..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
@@ -80,7 +58,7 @@ fisher install bass
 fisher install IlanCosman/tide@v6
 fisher install PatrickF1/fzf.fish
 
-$ echo "Installing fish catpuccin..."
+echo "Installing fish catpuccin..."
 fisher install catppuccin/fish
 
 echo "Starting Services..."
@@ -101,9 +79,6 @@ echo ".dotfiles" >>.gitignore
 git clone --bare https://github.com/collindutter $HOME/.dotfiles
 dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
-
-echo "Configuring tide prompt..."
-tide configure --auto --style=Lean --prompt_colors='True color' --show_time='12-hour format' --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
 
 echo "Configuring python virtualenvs..."
 mkdir .virtualenvs
