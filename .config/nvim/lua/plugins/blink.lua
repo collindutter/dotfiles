8a4844b4f8976d15d29ebcc987a007791ffcebe6
Collin Dutter <collin@griptape.ai>
2024-10-11 14:24:48 -0700

    Don't install blink nightly, update show keymap

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,25 +1,23 @@
 return {
   'saghen/blink.cmp',
   enabled = true,
-  build = 'cargo build --release',
+  -- build = 'cargo build --release',
+  version = 'v0.*',
   opts = {
     highlight = {
       use_nvim_cmp_as_default = true,
     },
     nerd_font_variant = 'normal',
-
     keymap = {
-      show = '<C-space>',
+      show = '<C-e>',
       hide = '<C-e>',
       accept = '<C-y>',
       select_prev = { '<C-p>' },
       select_next = { '<C-n>' },
-
       show_documentation = {},
       hide_documentation = {},
       scroll_documentation_up = '<C-u>',
       scroll_documentation_down = '<C-d>',
-
       snippet_forward = '<Tab>',
       snippet_backward = '<S-Tab>',
     },
