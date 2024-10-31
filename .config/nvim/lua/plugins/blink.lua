301857652046ce190bd1ad43aa8c01634be9855f
Collin Dutter <collin@griptape.ai>
2024-10-30 18:42:24 -0700

    Use default keymaps for blink

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -2,24 +2,13 @@ return {
   'saghen/blink.cmp',
   -- build = 'cargo build --release',
   version = 'v0.*',
+  enabled = true,
   opts = {
     highlight = {
       use_nvim_cmp_as_default = true,
     },
     nerd_font_variant = 'normal',
-    keymap = {
-      show = '<C-e>',
-      hide = '<C-e>',
-      accept = '<C-y>',
-      select_prev = { '<C-p>' },
-      select_next = { '<C-n>' },
-      show_documentation = {},
-      hide_documentation = {},
-      scroll_documentation_up = '<C-u>',
-      scroll_documentation_down = '<C-d>',
-      snippet_forward = '<Tab>',
-      snippet_backward = '<S-Tab>',
-    },
+    keymap = 'default',
     windows = {
       autocomplete = {
         border = 'rounded',
