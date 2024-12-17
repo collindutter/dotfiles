09cc0f5c3a22a7fd86e060dff348a650ad5f368a
Collin Dutter <collin@griptape.ai>
2024-12-17 08:57:18 -0800

    Update blink config

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -3,21 +3,38 @@ return {
   -- build = 'cargo build --release',
   version = 'v0.*',
   enabled = true,
+  opts_extend = { 'sources.completion.enabled_providers' },
   opts = {
+    sources = {
+      completion = {
+        enabled_providers = { 'lsp', 'path', 'snippets', 'buffer', 'codecompanion' },
+      },
+      providers = {
+        codecompanion = {
+          name = 'CodeCompanion',
+          module = 'codecompanion.providers.completion.blink',
+          enabled = true,
+        },
+      },
+    },
     highlight = {
       use_nvim_cmp_as_default = true,
     },
     nerd_font_variant = 'normal',
-    keymap = 'default',
-    windows = {
-      autocomplete = {
+    keymap = { preset = 'default' },
+    completion = {
+      menu = {
         border = 'rounded',
       },
-      signature_help = {
-        border = 'rounded',
+      signature = {
+        window = {
+          border = 'rounded',
+        },
       },
       documentation = {
-        border = 'rounded',
+        window = {
+          border = 'rounded',
+        },
         auto_show = true,
       },
     },
