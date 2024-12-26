ec5742d0eb1b326ba56d32d02f185038bd1f3916
Collin Dutter <collin@griptape.ai>
2024-12-26 12:35:27 -0800

    Update gitlinker

--- .config/nvim/lua/plugins/gitlinker.lua
+++ .config/nvim/lua/plugins/gitlinker.lua
@@ -1,15 +1,12 @@
 return {
-  -- Link to the current line on GitHub
-  'ruifm/gitlinker.nvim',
-  requires = 'nvim-lua/plenary.nvim',
-  opts = {
-    mappings = nil,
-  },
+  'linrongbin16/gitlinker.nvim',
+  opts = {},
   init = function()
     local map = require('helpers.keys').map
 
-    map('n', '<leader>gy', function()
-      require('gitlinker').get_repo_url()
-    end, '[g]it line [y]ank')
+    map('n', '<leader>gl', require('gitlinker').link, '[g]it [l]ink')
+    map('n', '<leader>gb', function()
+      require('gitlinker').link { router_type = 'blame' }
+    end, '[g]it [b]lame')
   end,
 }
