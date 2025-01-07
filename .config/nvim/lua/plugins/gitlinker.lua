756a0f24101ddd7d2a40edc62a12d21b89f6cabe
Collin Dutter <collindutter@gmail.com>
2025-01-07 15:06:30 -0800

    Fix gitlinter keybinds

--- .config/nvim/lua/plugins/gitlinker.lua
+++ .config/nvim/lua/plugins/gitlinker.lua
@@ -4,8 +4,8 @@ return {
   init = function()
     local map = require('helpers.keys').map
 
-    map('n', '<leader>gl', require('gitlinker').link, '[g]it [l]ink')
-    map('n', '<leader>gb', function()
+    map({ 'n', 'v' }, '<leader>gl', require('gitlinker').link, '[g]it [l]ink')
+    map({ 'n', 'v' }, '<leader>gb', function()
       require('gitlinker').link { router_type = 'blame' }
     end, '[g]it [b]lame')
   end,
