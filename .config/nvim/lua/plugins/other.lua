94387bf34a36367088e906d317a1ebe55bac0880
Collin Dutter <collin@griptape.ai>
2025-01-20 12:24:11 -0800

    Add border to other nvim

--- .config/nvim/lua/plugins/other.lua
+++ .config/nvim/lua/plugins/other.lua
@@ -1,14 +1,17 @@
 return {
   'rgroli/other.nvim',
-  main="other-nvim",
+  main = 'other-nvim',
   opts = {
     mappings = {
       'python',
+    },
+    style = {
+      border = 'rounded',
     },
   },
   init = function()
     local map = require('helpers.keys').map
 
-    map('n', '<leader>o', "<cmd>Other<cr>", '[o]ther')
+    map('n', '<leader>o', '<cmd>Other<cr>', '[o]ther')
   end,
 }
