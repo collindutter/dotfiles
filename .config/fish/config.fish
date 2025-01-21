c61d551c9782fa8f4de121169c99bbed28018450
Collin Dutter <collin@griptape.ai>
2025-01-20 20:32:48 -0800

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,16 +8,28 @@ fish_add_path /opt/homebrew/bin
 fish_add_path ~/.local/share/
 
 if status --is-interactive
-	fish_vi_key_bindings
-	set -g fish_vi_force_cursor 1
 
+	# https://fishshell.com/docs/current/interactive.html#vi-mode-commands
+	# Enable vim mode
+	fish_vi_key_bindings
+	# Have the cursor change shape in different modes
+	fish_vi_cursor
+	# All required to make this work in tmux
+	set -g fish_vi_force_cursor 1
 	set fish_cursor_default block
 	set fish_cursor_insert line
 	set fish_cursor_replace_one underscore
+	set fish_cursor_replace underscore
+	set fish_cursor_external line
+	set fish_cursor_visual block
 
+	# Leave insert mode when pressing jk
 	bind -M insert -m default jk backward-char force-repaint
 
+	# Disable the default fish greeting
 	set fish_greeting
+	
+	# Improve fzf key bindings
 	fzf_configure_bindings --directory=\cf --git_log=\cl
 	
 	fish_config theme choose "Catppuccin Macchiato"
