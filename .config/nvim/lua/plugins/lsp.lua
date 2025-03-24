61180cf7726b06b019604e6ec48d3e143fa0bfd0
Collin Dutter <collin@griptape.ai>
2025-03-24 08:54:51 -0700

    Add lsp high cpu workaround

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -52,6 +52,70 @@ return {
       -- Make diagnostics pretty
       vim.diagnostic.config(opts.diagnostic_config)
 
+      local watch_type = require('vim._watch').FileChangeType
+
+      -- Hack for high CPU https://github.com/neovim/neovim/issues/23291#issuecomment-1712422887
+      local function handler(res, callback)
+        if not res.files or res.is_fresh_instance then
+          return
+        end
+
+        for _, file in ipairs(res.files) do
+          local path = res.root .. '/' .. file.name
+          local change = watch_type.Changed
+          if file.new then
+            change = watch_type.Created
+          end
+          if not file.exists then
+            change = watch_type.Deleted
+          end
+          callback(path, change)
+        end
+      end
+
+      local function watchman(path, _, callback)
+        vim.system({ 'watchman', 'watch', path }):wait()
+
+        local buf = {}
+        local sub = vim.system({
+          'watchman',
+          '-j',
+          '--server-encoding=json',
+          '-p',
+        }, {
+          stdin = vim.json.encode {
+            'subscribe',
+            path,
+            'nvim:' .. path,
+            {
+              expression = { 'anyof', { 'type', 'f' }, { 'type', 'd' } },
+              fields = { 'name', 'exists', 'new' },
+            },
+          },
+          stdout = function(_, data)
+            if not data then
+              return
+            end
+            for line in vim.gsplit(data, '\n', { plain = true, trimempty = true }) do
+              table.insert(buf, line)
+              if line == '}' then
+                local res = vim.json.decode(table.concat(buf))
+                handler(res, callback)
+                buf = {}
+              end
+            end
+          end,
+          text = true,
+        })
+
+        return function()
+          sub:kill 'sigint'
+        end
+      end
+
+      if vim.fn.executable 'watchman' == 1 then
+        require('vim.lsp._watchfiles')._watchfunc = watchman
+      end
     end,
   },
   {
@@ -98,6 +162,7 @@ return {
         ts_ls = {},
         html = {},
         typos_lsp = {},
+        harper_ls = {},
         jinja_lsp = {
           filetypes = { 'jinja', 'jinja2', 'j2', 'htmldjango' },
         },
@@ -190,8 +255,8 @@ return {
     config = function(_, opts)
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       -- Blink enables more LSP capabilities than default Neovim
-      local ok, blink = pcall(require, 'blink.cmp')
-      if ok then
+      local blink_ok, blink = pcall(require, 'blink.cmp')
+      if blink_ok then
         capabilities = blink.get_lsp_capabilities(capabilities)
       end
       -- TODO: remove when fixed: https://github.com/neovim/neovim/issues/23291
