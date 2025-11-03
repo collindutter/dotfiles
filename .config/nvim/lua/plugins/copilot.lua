b41b26dc640208fdfcda17f216eb90fa0b0bd297
Collin Dutter <collin@griptape.ai>
2025-11-03 10:51:17 -0800

    refactor: clean up copilot usage

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,29 +1,15 @@
 return {
   -- Copilot
   'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
   event = 'InsertEnter',
   opts = {
     panel = { enabled = false },
     suggestion = {
       auto_trigger = true,
-      accept = false,
       keymap = {
         accept = '<C-l>',
       },
-    },
-    filetypes = {
-      yaml = true,
-      markdown = true,
-      help = false,
-      gitcommit = false,
-      gitrebase = false,
-      hgcommit = false,
-      svn = false,
-      cvs = false,
-      ['grug-far'] = false,
-      ['grug-far-history'] = false,
-      ['grug-far-help'] = false,
-      ['.'] = false,
     },
   },
 }
