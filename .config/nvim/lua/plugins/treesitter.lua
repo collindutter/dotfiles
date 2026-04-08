d718b566a3d2b2dcc00c3605a3bddc705c64a502
Collin Dutter <collin@griptape.ai>
2026-04-08 08:55:45 -0700

    feat(treesitter): auto install treesitter parsers

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,31 @@
-return {
-  -- Highlight, edit, and navigate code
+return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  lazy = false,
   build = ':TSUpdate',
+  branch = 'main',
   config = function()
     vim.api.nvim_create_autocmd('FileType', {
+      pattern = { '*' },
       callback = function(args)
-        pcall(vim.treesitter.start, args.buf)
+        local ft = vim.bo[args.buf].filetype
+        local lang = vim.treesitter.language.get_lang(ft)
+
+        if not vim.treesitter.language.add(lang) then
+          local available = vim.g.ts_available or require('nvim-treesitter').get_available()
+          if not vim.g.ts_available then
+            vim.g.ts_available = available
+          end
+          if vim.tbl_contains(available, lang) then
+            require('nvim-treesitter').install(lang)
+          end
+        end
+
+        if vim.treesitter.language.add(lang) then
+          vim.treesitter.start(args.buf, lang)
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          vim.wo[0][0].foldmethod = 'expr'
+        end
       end,
     })
   end,
