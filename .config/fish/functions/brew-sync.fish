5a412b1aa4b576aacc037d87b2a1f650daeacf80
Collin Dutter <collin@griptape.ai>
2026-07-15 09:40:01 -0700

    fix(brew-sync): auto-confirm brew upgrade with --yes

--- .config/fish/functions/brew-sync.fish
+++ .config/fish/functions/brew-sync.fish
@@ -1,3 +1,3 @@
 function brew-sync --wraps='brew update && brew bundle install --force-cleanup --file=~/.config/homebrew/Brewfile && brew upgrade' --description 'alias brew-sync brew update && brew bundle install --force-cleanup --file=~/.config/homebrew/Brewfile && brew upgrade'
-  brew update && brew bundle install --force-cleanup --file=~/.config/homebrew/Brewfile && brew upgrade $argv
+  brew update && brew bundle install --force-cleanup --file=~/.config/homebrew/Brewfile && brew upgrade --yes $argv
 end
